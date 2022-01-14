351c9ae643ba2cb70bb22627799a6e20f435e7b2
Listopadov Evgeny <Listopadov_94@mail.ru>
2022-01-14 15:03:04 +0300

    Добавил класс Bot

---
+++ 01Weapon/Bot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Weapon
+{
+    public class Bot
+    {
+        private readonly Weapon Weapon;
+
+        public Bot(Weapon weapon)
+        {
+            Weapon = weapon;
+        }
+
+        public void OnSeePlayer(Player player)
+        {
+            if (player.Dead)
+                return;
+
+            Weapon.Fire(player);
+        }
+    }
+}
